f8e11113035178cd9de6fb2ea3cf949d75f489c5
Daniil Baturin <daniil@baturin.org>
2015-02-13 23:46:48 +0600

    Add the first actual unit test.

--- test/vyconf_tree_test.ml
+++ test/vyconf_tree_test.ml
@@ -1,10 +1,17 @@
 open OUnit2
+open Vytree
 
 let test1 test_ctxt = assert_equal 0 0
 
+let test_make_node test_ctxt =
+    let node = make "root" () in
+    assert_equal (name_of_node node) "root";
+    assert_equal (data_of_node node) ();
+    assert_equal (children_of_node node) []
+
 let suite =
     "VyConf tree tests" >::: [
-        "test_dummy">:: test1;
+        "test_make_node" >:: test_make_node;
     ]
 
 let () =
