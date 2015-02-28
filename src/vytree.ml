559715be78fdfa5d6f3a1756dfa0b8ebb7032228
Daniil Baturin <daniil@baturin.org>
2015-02-28 06:22:12 +0600

    Fix the notion of duplicate child in insert_child. Right now it doesn't compare data, but maybe it should.

--- src/vytree.ml
+++ src/vytree.ml
@@ -71,14 +71,20 @@ let list_children node =
 let rec insert_child default_data node path data =
     match path with
     | [] -> raise Empty_path
-    | [name] -> insert_immediate_child node name data
+    | [name] ->
+        begin
+            (* Check for duplicate item *)
+            let last_child = find_child node name in
+            match last_child with
+            | None -> insert_immediate_child node name data
+            | (Some _) -> raise Duplicate_child
+        end
     | name :: names ->
         let next_child = find_child node name in
         match next_child with
         | Some next_child' ->
             let new_node = insert_child default_data next_child' names data in
-            if names = [] then raise Duplicate_child
-            else replace_child node new_node
+            replace_child node new_node
         | None ->
             let next_child' = make name default_data in
             let new_node = insert_child default_data next_child' names data in
