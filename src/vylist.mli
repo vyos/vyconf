ef4e6d0bf2e62fd854533fa135ed96247b94d1ff
Daniil Baturin <daniil@baturin.org>
2015-03-28 08:46:29 +0600

    Add insert_before and insert_after to the Vylist signature.

--- src/vylist.mli
+++ src/vylist.mli
@@ -1,3 +1,5 @@
 val find : ('a -> bool) -> 'a list -> 'a option
 val remove : ('a -> bool) -> 'a list -> 'a list
 val replace : ('a -> bool) -> 'a -> 'a list -> 'a list
+val insert_before : ('a -> bool) -> 'a -> 'a list -> 'a list
+val insert_after : ('a -> bool) -> 'a -> 'a list -> 'a	list
