3a5148b6535d2446be0b7fc3599fd668d927409b
Daniil Baturin <daniil@baturin.org>
2015-03-28 06:25:58 +0600

    Add insert_before and insert_after functions to Vylist. They will be used in node reordering.

--- src/vylist.ml
+++ src/vylist.ml
@@ -15,3 +15,15 @@ let rec replace p x xs =
     | [] -> []
     | x' :: xs' -> if (p x') then x :: xs'
                    else x' :: (replace p x xs')
+
+let rec insert_before p x xs =
+    match xs with
+    | [] -> raise Not_found
+    | y :: ys -> if (p y) then x :: y :: ys
+                 else y :: (insert_before p x ys)
+
+let rec insert_after p x xs =
+    match xs with
+    | [] -> raise Not_found
+    | y :: ys -> if (p y) then y :: x :: ys
+                 else y :: (insert_after p x ys)
