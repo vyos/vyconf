274c49d89623b95f8b682b8dba5e9db9174ec808
Daniil Baturin <daniil@baturin.org>
2015-10-01 22:32:12 +0600

    Some error handling in config loading.

--- src/vyconfd.ml
+++ src/vyconfd.ml
@@ -1,5 +1,15 @@
+open Lwt
 open Defaults
 
-let config = Vyconf_config.load defaults.config_file
+let () = Lwt_log.add_rule "*" Lwt_log.Info
+
+let config =
+    let result = Vyconf_config.load defaults.config_file in
+    match result with
+    | `Ok cfg -> cfg
+    | `Error err ->
+        Lwt_log.fatal (Printf.sprintf "Could not load the configuration file %s" err); exit 1
+
+
 
 let () = print_endline "This is VyConf. Or, rather, will be."
