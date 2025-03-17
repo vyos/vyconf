d246228cfb94261a4b71cd8ed066d6c651f0a550
John Estabrook <jestabro@vyos.io>
2025-03-16 22:45:18 -0500

    T7121: add cache directory and files, and commitd socket name
    Pass active and proposed configs between vyconfd and vyos-commitd using
    a fast cache to avoid parsing overhead.

--- src/vyconf_config.mli
+++ src/vyconf_config.mli
@@ -4,9 +4,12 @@ type t = {
     program_dir: string;
     config_dir: string;
     reftree_dir: string;
+    session_dir: string;
     primary_config: string;
     fallback_config: string;
     reference_tree: string;
+    running_cache: string;
+    session_cache: string;
     socket: string;
     pid_file: string;
     log_file: string option;
